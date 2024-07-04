fa59d14125e4d608e326d59e841e3e1e0a57d7b7
Chanakya Das Sahu <ccccsahu@gmail.com>
2024-07-04 12:14:46 +0530

    cart , filter , localStorage auth , middleware auth , .env , .gitignore , detailPage

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ require('dotenv').config({path:'./.env'})
 const mongoURI = process.env.MONGO_URI
 const connectToMongo = () =>{
     mongoose.connect(mongoURI)
-    .then(()=>{console.log('connected')})
+    .then(()=>{console.log('connected')}) //??
     .catch((err)=>{console.log(`not connected due to err : ${err}`)})
 }
 
